ece70b9b5679914f79037c24eea93a401320e5e9
Jessica <jessica.code99@gmail.com>
2023-01-17 19:21:42 +0530

    Initial commit

---
+++ build/precache-manifest.f847baae22c699bc66fca6bd3d28ea8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfbdf5d1515532a6f843",
+    "url": "/static/css/main.68f3f110.chunk.css"
+  },
+  {
+    "revision": "dfbdf5d1515532a6f843",
+    "url": "/static/js/main.dfbdf5d1.chunk.js"
+  },
+  {
+    "revision": "25ca0dc680155bae9ec1",
+    "url": "/static/js/1.25ca0dc6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c69f01b822f296abbb171c7b28bf2ffd",
+    "url": "/index.html"
+  }
+];
